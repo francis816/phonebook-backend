d42c87f0e5fd859776353a67d94e6ee8f8dbee7b
Francis <92971421+francis816@users.noreply.github.com>
2022-01-24 16:17:39 -0600

    3.15-3.18: link more routes to db, error middlewar

--- index.js
+++ index.js
@@ -22,60 +22,39 @@ app.use(requestLogger);
 
 morgan.token("body", request => JSON.stringify(request.body));
 
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
+    Person
+        .find({})
+        .then(persons => {
+            response.json(persons)
+        })
 })
 
 app.get('/info', (request, response) => {
-    response.send(`<p>Phonebook has info for ${persons.length} people</p> ${new Date()}`)
+    Person
+        .find({})
+        .then(persons => {
+            response.send(`<p>Phonebook has info for ${persons.length} people</p> ${new Date()}`)
+        })
+
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    // const id = Number(request.params.id)
-    // const person = persons.find(person => {
-    //     return person.id === id
-    // })
-    // if (person) {
-
-    // } else {
-    //     response.status(404).end()
-    // }
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+    Person
+        .findById(request.params.id)
+        .then(person => {
+            response.json(person)
+        })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => {
-        return person.id !== id
-    })
-    response.status(204).end()
+    Person
+        .findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 
@@ -86,45 +65,57 @@ app.post('/api/persons', (request, response) => {
             error: "name missing"
         })
     }
-    const personExists = name => {
-        return persons.find(person => {
-            return person.name === name
-        })
-    }
 
-    if (personExists(body.name)) {
-        return response.status(400).json({
-            error: "name already exists"
-        })
-    }
     if (!body.number) {
         return response.status(400).json({
             error: "number missing"
         })
     }
     const person = new Person({
-        "id": Math.floor(Math.random() * 1000000000),
+        // "id": Math.floor(Math.random() * 1000000000),
         "name": body.name,
         "number": body.number
     })
 
-
-    // persons = persons.concat(person)
-
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    person
+        .save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
 
 })
 
+// update phone number if user existed
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+    Person
+        .findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
 
-// e.g. if someone tries to go to localhost:3001/akjkajg
-// we will send him an error in JSON on the page 
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
-
 app.use(unknownEndpoint)
+
+
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === "CastError") {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+app.use(errorHandler)
 
 
 
